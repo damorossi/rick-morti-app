971dd76f7a6fab52a569e1895aae32639cef831a
Damian <damianrossi@live.com.ar>
2021-10-25 16:58:01 +0200

    Remove app requirement

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import './index.scss';
-import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 import { AppRouter } from './routes/AppRouting';
